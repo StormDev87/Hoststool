acb5260c628637273e2efcd21aeb0d303dde7084
stormdev87 <stormdev87@gmail.com>
2023-09-08 18:37:48 +0200

    Initial Commit

---
+++ Hosts Tool/Hosts_ToolApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hosts_ToolApp.swift
+//  Hosts Tool
+//
+//  Created by Matteo Visca on 08/09/23.
+//
+
+import SwiftUI
+
+@main
+struct Hosts_ToolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
